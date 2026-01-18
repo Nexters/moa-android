b3ebaaaeae71309867fbe09ff2a487291f9f50c6
koreatlwls <koreatlwls@gmail.com>
2026-01-18 22:30:07 +0900

    remove : import

--- presentation/src/main/kotlin/com/moa/app/presentation/ui/splash/SplashScreen.kt
+++ presentation/src/main/kotlin/com/moa/app/presentation/ui/splash/SplashScreen.kt
@@ -1,14 +1,11 @@
 package com.moa.app.presentation.ui.splash
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
