d094fc538d2e3e349de69938e4adf2b5b896e6c8
koreatlwls <koreatlwls@gmail.com>
2026-01-20 21:13:57 +0900

    feat : asSharedFlow 추가

--- presentation/src/main/kotlin/com/moa/app/presentation/bus/MoaSideEffectBus.kt
+++ presentation/src/main/kotlin/com/moa/app/presentation/bus/MoaSideEffectBus.kt
@@ -2,13 +2,14 @@ package com.moa.app.presentation.bus
 
 import com.moa.app.presentation.model.MoaSideEffect
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class MoaSideEffectBus @Inject constructor() {
     private val _sideEffects = MutableSharedFlow<MoaSideEffect>()
-    val sideEffects = _sideEffects
+    val sideEffects = _sideEffects.asSharedFlow()
 
     suspend fun emit(sideEffect: MoaSideEffect) {
         _sideEffects.emit(sideEffect)
