38fa5c7ae98b262ec2c8eab0e458beea26d13e54
koreatlwls <koreatlwls@gmail.com>
2026-01-22 20:45:21 +0900

    update : 파일 분리

--- presentation/src/main/kotlin/com/moa/app/presentation/designsystem/component/MoaTextField.kt
+++ presentation/src/main/kotlin/com/moa/app/presentation/designsystem/component/MoaTextField.kt
@@ -1,12 +1,9 @@
 package com.moa.app.presentation.designsystem.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,7 +11,6 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.InputTransformation
 import androidx.compose.foundation.text.input.OutputTransformation
-import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,78 +19,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.moa.app.presentation.designsystem.theme.MoaTheme
-
-@Composable
-fun MoaTextFieldWithDescription(
-    modifier: Modifier = Modifier,
-    description1: String,
-    description2: String,
-    state: TextFieldState,
-    placeholder: String,
-    inputTransformation: InputTransformation? = null,
-) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(
-            text = description1,
-            color = MoaTheme.colors.textHighEmphasis,
-            style = MoaTheme.typography.t1_700,
-        )
-
-        Spacer(Modifier.height(8.dp))
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(70.dp)
-                .background(
-                    color = MoaTheme.colors.containerPrimary,
-                    shape = RoundedCornerShape(MoaTheme.radius.radius16),
-                ),
-            contentAlignment = Alignment.Center,
-        ) {
-            BasicTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = MoaTheme.spacing.spacing20),
-                state = state,
-                textStyle = MoaTheme.typography.h3_700.copy(
-                    color = MoaTheme.colors.textGreen,
-                    textAlign = TextAlign.Center,
-                ),
-                lineLimits = TextFieldLineLimits.SingleLine,
-                cursorBrush = SolidColor(Color.White),
-                inputTransformation = inputTransformation,
-                decorator = { innerTextField ->
-                    if (state.text.isEmpty()) {
-                        Text(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = placeholder,
-                            style = MoaTheme.typography.h3_700,
-                            color = MoaTheme.colors.textDisabled,
-                            textAlign = TextAlign.Center,
-                        )
-                    }
-                    innerTextField()
-                }
-            )
-        }
-
-        Spacer(Modifier.height(8.dp))
-
-        Text(
-            text = description2,
-            color = MoaTheme.colors.textHighEmphasis,
-            style = MoaTheme.typography.t1_700,
-        )
-    }
-}
 
 @Composable
 fun MoaFilledTextField(
@@ -102,7 +29,7 @@ fun MoaFilledTextField(
     modifier: Modifier = Modifier,
     placeholder: String,
     trailingText: String,
-    keyboardOptions : KeyboardOptions = KeyboardOptions.Default,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     inputTransformation: InputTransformation? = null,
     outputTransformation: OutputTransformation? = null,
 ) {
@@ -150,19 +77,6 @@ fun MoaFilledTextField(
         )
     }
 
-}
-
-@Preview(backgroundColor = 0xFF000000, showBackground = true)
-@Composable
-private fun MoaTextFieldWithDescriptionPreview() {
-    MoaTheme {
-        MoaTextFieldWithDescription(
-            description1 = "닉네임",
-            description2 = "로 가입할래요",
-            state = TextFieldState(),
-            placeholder = "닉네임을 입력해주세요",
-        )
-    }
 }
 
 @Preview(backgroundColor = 0xFF000000, showBackground = true)
---
+++ presentation/src/main/kotlin/com/moa/app/presentation/designsystem/component/MoaTimeBottomSheet.kt
@@ -0,0 +1,301 @@
+package com.moa.app.presentation.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.moa.app.core.makeTimeString
+import com.moa.app.presentation.R
+import com.moa.app.presentation.designsystem.theme.MoaTheme
+import com.moa.app.presentation.model.Time
+import kotlinx.collections.immutable.toImmutableList
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MoaTimeBottomSheet(
+    time: Time,
+    visible: Boolean,
+    onClickButton: (startHour: Int, startMinute: Int, endHour: Int, endMinute: Int) -> Unit,
+    onDismissRequest: () -> Unit,
+) {
+    MoaBottomSheet(
+        visible = visible,
+        onDismissRequest = onDismissRequest,
+        sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    ) {
+        MoaTimeBottomSheetContent(
+            time = time,
+            onClickButton = onClickButton,
+            onDismissRequest = onDismissRequest,
+        )
+    }
+}
+
+@Composable
+private fun MoaTimeBottomSheetContent(
+    time: Time,
+    onClickButton: (startHour: Int, startMinute: Int, endHour: Int, endMinute: Int) -> Unit,
+    onDismissRequest: () -> Unit,
+) {
+    var selectedStartTime by remember { mutableStateOf(true) }
+    var startHour by remember { mutableIntStateOf(time.startHour) }
+    var startMinute by remember { mutableIntStateOf(time.startMinute) }
+    var endHour by remember { mutableIntStateOf(time.endHour) }
+    var endMinute by remember { mutableIntStateOf(time.endMinute) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = MoaTheme.spacing.spacing20)
+    ) {
+        MoaTimeBottomSheetTitleContent(time = time)
+
+        Spacer(Modifier.height(MoaTheme.spacing.spacing16))
+
+        MoaTimeBottomSheetTimeContent(
+            time = time,
+            selectedStartTime = selectedStartTime,
+            startHour = startHour,
+            startMinute = startMinute,
+            endHour = endHour,
+            endMinute = endMinute,
+            onSelectedHour = { selectedHour ->
+                if (selectedStartTime) {
+                    startHour = selectedHour
+                } else {
+                    endHour = selectedHour
+                }
+            },
+            onSelectedMinute = { selectedMinute ->
+                if (selectedStartTime) {
+                    startMinute = selectedMinute
+                } else {
+                    endMinute = selectedMinute
+                }
+            },
+            onClickStartTime = { selectedStartTime = true },
+            onClickEndTime = { selectedStartTime = false },
+        )
+
+        MoaPrimaryButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = MoaTheme.spacing.spacing20)
+                .height(64.dp),
+            enabled = if (selectedStartTime) {
+                true
+            } else {
+                (endHour > startHour) || (endHour == startHour && endMinute > startMinute)
+            },
+            onClick = {
+                if (selectedStartTime) {
+                    selectedStartTime = false
+                } else {
+                    onClickButton(startHour, startMinute, endHour, endMinute)
+                    onDismissRequest()
+                }
+            },
+        ) {
+            Text(
+                text = if (selectedStartTime) {
+                    time.startButtonText
+                } else {
+                    time.endButtonText
+                },
+                style = MoaTheme.typography.t3_700,
+            )
+        }
+    }
+}
+
+@Composable
+private fun MoaTimeBottomSheetTitleContent(
+    time: Time,
+) {
+    Text(
+        text = when (time) {
+            is Time.Work -> "${time.title}을 알려주세요"
+            is Time.Lunch -> "${time.title}을 알려주세요"
+        },
+        style = MoaTheme.typography.t1_700,
+        color = MoaTheme.colors.textHighEmphasis,
+    )
+
+    if (time.description.isNotEmpty()) {
+        Spacer(Modifier.height(MoaTheme.spacing.spacing4))
+
+        Text(
+            text = time.description,
+            style = MoaTheme.typography.b1_400,
+            color = MoaTheme.colors.textMediumEmphasis,
+        )
+    }
+}
+
+@Composable
+private fun MoaTimeBottomSheetTimeContent(
+    time: Time,
+    selectedStartTime: Boolean,
+    startHour: Int,
+    startMinute: Int,
+    endHour: Int,
+    endMinute: Int,
+    onSelectedHour: (Int) -> Unit,
+    onSelectedMinute: (Int) -> Unit,
+    onClickStartTime: () -> Unit,
+    onClickEndTime: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = MoaTheme.spacing.spacing24),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .clickable { onClickStartTime() },
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = when (time) {
+                    is Time.Work -> "출근"
+                    is Time.Lunch -> "시작"
+                },
+                style = MoaTheme.typography.b2_500,
+                color = MoaTheme.colors.textLowEmphasis,
+            )
+
+            Text(
+                text = makeTimeString(startHour, startMinute),
+                style = if (selectedStartTime) MoaTheme.typography.t1_700 else MoaTheme.typography.t1_500,
+                color = if (selectedStartTime) MoaTheme.colors.textGreen else MoaTheme.colors.textLowEmphasis,
+            )
+        }
+
+        Icon(
+            modifier = Modifier.padding(horizontal = MoaTheme.spacing.spacing24),
+            painter = painterResource(R.drawable.icon_arrow_right),
+            contentDescription = "ArrowRight",
+            tint = MoaTheme.colors.textLowEmphasis,
+        )
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .clickable { onClickEndTime() },
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = when (time) {
+                    is Time.Work -> "퇴근"
+                    is Time.Lunch -> "종료"
+                },
+                style = MoaTheme.typography.b2_500,
+                color = MoaTheme.colors.textLowEmphasis,
+            )
+
+            Text(
+                text = makeTimeString(endHour, endMinute),
+                style = if (!selectedStartTime) MoaTheme.typography.t1_700 else MoaTheme.typography.t1_500,
+                color = if (!selectedStartTime) MoaTheme.colors.textGreen else MoaTheme.colors.textLowEmphasis,
+            )
+        }
+    }
+
+    Spacer(Modifier.height(MoaTheme.spacing.spacing16))
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(236.dp)
+            .clip(RoundedCornerShape(MoaTheme.radius.radius16))
+            .background(
+                color = MoaTheme.colors.containerSecondary,
+                shape = RoundedCornerShape(MoaTheme.radius.radius16),
+            )
+            .padding(MoaTheme.spacing.spacing8),
+        contentAlignment = Alignment.Center,
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(52.dp)
+                .background(
+                    color = MoaTheme.colors.containerPrimary,
+                    shape = RoundedCornerShape(MoaTheme.radius.radius12)
+                )
+        )
+
+        Row {
+            key(selectedStartTime) {
+                MoaWheelPicker(
+                    modifier = Modifier.width(120.dp),
+                    items = (0..23).toList().toImmutableList(),
+                    initialSelectedIndex = if (selectedStartTime) {
+                        startHour
+                    } else {
+                        endHour
+                    },
+                    onItemSelected = onSelectedHour,
+                )
+
+                MoaWheelPicker(
+                    modifier = Modifier.width(120.dp),
+                    items = (0..59).toList().toImmutableList(),
+                    initialSelectedIndex = if (selectedStartTime) {
+                        startMinute
+                    } else {
+                        endMinute
+                    },
+                    onItemSelected = onSelectedMinute,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun MoaTimeBottomSheetPreview() {
+    MoaTheme {
+        Column(Modifier.fillMaxSize()) {
+            MoaTimeBottomSheet(
+                time = Time.Work(
+                    startHour = 9,
+                    startMinute = 0,
+                    endHour = 18,
+                    endMinute = 0,
+                ),
+                visible = true,
+                onClickButton = { _, _, _, _ -> },
+                onDismissRequest = {}
+            )
+        }
+    }
+}
