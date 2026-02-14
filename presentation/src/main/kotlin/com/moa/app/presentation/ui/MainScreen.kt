78b71aa89db41278b2ec4e48647c187ed1725abc
koreatlwls <koreatlwls@gmail.com>
2026-02-14 15:18:56 +0900

    remove : import

--- presentation/src/main/kotlin/com/moa/app/presentation/ui/MainScreen.kt
+++ presentation/src/main/kotlin/com/moa/app/presentation/ui/MainScreen.kt
@@ -1,10 +1,5 @@
 package com.moa.app.presentation.ui
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
