16fd82f042e169a7efbb296caadfe435641f0160
koreatlwls <koreatlwls@gmail.com>
2026-02-14 15:32:30 +0900

    update : lottie size

--- presentation/src/main/kotlin/com/moa/app/presentation/designsystem/component/MoaFullScreenProgress.kt
+++ presentation/src/main/kotlin/com/moa/app/presentation/designsystem/component/MoaFullScreenProgress.kt
@@ -3,11 +3,13 @@ package com.moa.app.presentation.designsystem.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -35,6 +37,7 @@ fun MoaFullScreenProgress() {
         contentAlignment = Alignment.Center,
     ) {
         LottieAnimation(
+            modifier = Modifier.size(96.dp),
             composition = composition,
             progress = { progress }
         )
