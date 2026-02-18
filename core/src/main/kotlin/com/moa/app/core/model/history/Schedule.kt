fafc5fc8e766abf6d0457e7f51bae90d81d568b4
yeonjeen <jeendev2019@gmail.com>
2026-02-18 20:42:47 +0900

    feat: add history domain models

---
+++ core/src/main/kotlin/com/moa/app/core/model/history/Schedule.kt
@@ -0,0 +1,25 @@
+package com.moa.app.core.model.history
+
+import androidx.compose.runtime.Stable
+import com.moa.app.core.model.onboarding.Time
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Stable
+data class Schedule(
+    val id: Long,
+    val date: LocalDateModel,
+    val type: ScheduleType,
+    val time: Time? = null,
+    val amount: Long? = null,
+)
+
+@Serializable
+@Stable
+data class LocalDateModel(
+    val year: Int,
+    val month: Int,
+    val day: Int,
+) {
+    fun toDisplayString(): String = "${year}년 ${month}월 ${day}일"
+}
