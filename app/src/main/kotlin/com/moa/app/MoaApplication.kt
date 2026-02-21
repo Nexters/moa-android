273855d57dcd12a2ad1302ff1c203a2dbcb2762c
koreatlwls <koreatlwls@gmail.com>
2026-02-21 22:11:53 +0900

    feat : FCM 노티 띄우기

--- app/src/main/kotlin/com/moa/app/MoaApplication.kt
+++ app/src/main/kotlin/com/moa/app/MoaApplication.kt
@@ -1,14 +1,29 @@
 package com.moa.app
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MoaApplication: Application() {
+class MoaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            "moaChannelId",
+            "Moa",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+
+        val notificationManager: NotificationManager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
