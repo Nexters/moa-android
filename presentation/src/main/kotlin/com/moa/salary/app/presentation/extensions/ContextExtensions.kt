6de59adcc08e36a66b839f2a86a8a3ea7d8fcd5e
koreatlwls <koreatlwls@gmail.com>
2026-02-25 21:36:26 +0900

    feat : 알림 설정에서 시스템 알림 설정으로 보내기

--- presentation/src/main/kotlin/com/moa/salary/app/presentation/extensions/ContextExtensions.kt
+++ presentation/src/main/kotlin/com/moa/salary/app/presentation/extensions/ContextExtensions.kt
@@ -3,6 +3,7 @@ package com.moa.salary.app.presentation.extensions
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.provider.Settings
 import androidx.core.net.toUri
 
 fun Context.sendEmail(appVersion: String) {
@@ -30,4 +31,12 @@ fun Context.sendEmail(appVersion: String) {
     } catch (_: ActivityNotFoundException) {
 
     }
+}
+
+fun Context.openNotificationSettings() {
+    val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+        putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+    }
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    startActivity(intent)
 }
