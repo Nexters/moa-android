6dfbcf31d35828b08c726c1fb21c99d50ce6e16f
koreatlwls <koreatlwls@gmail.com>
2026-03-14 16:40:18 +0900

    update : dialog spacing

--- presentation/src/main/kotlin/com/moa/salary/app/presentation/designsystem/component/MoaDialog.kt
+++ presentation/src/main/kotlin/com/moa/salary/app/presentation/designsystem/component/MoaDialog.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -126,7 +127,7 @@ private fun ConfirmButtonRow(
             )
         }
 
-        Spacer(modifier = Modifier.padding(MoaTheme.spacing.spacing12))
+        Spacer(modifier = Modifier.width(MoaTheme.spacing.spacing8))
 
         MoaPrimaryButton(
             modifier = Modifier
