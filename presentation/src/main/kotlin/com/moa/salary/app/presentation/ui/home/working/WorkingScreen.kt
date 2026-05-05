2545a3f9085d3b8f210ebc65e28fbb9238f34d25
koreatlwls <koreatlwls@gmail.com>
2026-05-05 17:43:05 +0900

    fix : 누적월급 계산

--- presentation/src/main/kotlin/com/moa/salary/app/presentation/ui/home/working/WorkingScreen.kt
+++ presentation/src/main/kotlin/com/moa/salary/app/presentation/ui/home/working/WorkingScreen.kt
@@ -791,20 +791,7 @@ private fun WorkingScreenPreview() {
         ) {
             WorkingScreen(
                 uiState = WorkingUiState(
-                    args = HomeNavigation.Working(
-                        home = Home(
-                            workplace = "모아주식회사",
-                            workedEarnings = 1000000,
-                            standardSalary = 1000000,
-                            dailyPay = 100000,
-                            type = WorkdayType.VACATION,
-                            events = persistentListOf(),
-                            startHour = 9,
-                            startMinute = 0,
-                            endHour = 18,
-                            endMinute = 0,
-                        ),
-                    ),
+                    completed = false,
                     home = Home(
                         workplace = "모아주식회사",
                         workedEarnings = 1000000,
